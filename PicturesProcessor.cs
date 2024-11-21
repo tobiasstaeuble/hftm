72d77ece232a59c6b6591eafe45541c04fa67f70
tobias staeuble <tobias.staeuble@bdo.ch>
2024-11-21 14:29:44 +0100

    try use opencv for face detection

--- PicturesProcessor.cs
+++ PicturesProcessor.cs
@@ -15,11 +15,21 @@ using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 using System;
 using System.IO;
+using System.Text;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using Emgu.CV;
+using System.Drawing;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.Util;
+using Emgu.CV.UI;
 
 namespace HFTM.PictureProcessor
 {
     public static class PicturesProcessor
     {
+        private static HaarDetectionType _detectionType = HaarDetectionType.DoCannyPruning;
         static ILogger logger = null;
 
         [FunctionName(nameof(PicturesProcessor))]
@@ -42,6 +52,36 @@ namespace HFTM.PictureProcessor
             // determine extension of document to be uploaded
             var extension = GetUploadedDocumentExtension(eventGridEvent);
             logger.LogInformation($"File extension: {extension}");
+
+            // fetch new photo from storage
+            var newPhoto = await GetUploadedPhotoFromStorageAsStream(eventGridEvent, blobServiceClient);
+
+            // load into magick.net
+            var image = SixLabors.ImageSharp.Image.Load(newPhoto);
+
+            // convert to png
+            Stream outputStreamPng = new MemoryStream();
+            try
+            {
+                var encoder = new PngEncoder();
+                image.Save(outputStreamPng, encoder);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Failed to convert image to png.");
+                logger.LogError(ex.Message);
+            }
+
+            Bitmap bitmap = new Bitmap(outputStreamPng);
+            var cvImg = bitmap.ToImage<Bgr, byte>();
+            Image<Gray, byte> grayframe = cvImg.Convert<Gray, byte>();
+            CascadeClassifier _cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_default.xml");
+
+            var rectangles = _cascadeClassifier.DetectMultiScale(grayframe, 1.1, 10, new Size(20, 20));
+
+            logger.LogInformation($"Detected {rectangles.Length} faces.");
+            logger.LogInformation($"First face rectangle coordinates: {rectangles[0].X}/{rectangles[0].Y} Size: {rectangles[0].Width}/{rectangles[0].Height}");
+
 
         }
 
@@ -75,6 +115,18 @@ namespace HFTM.PictureProcessor
             downloadCertificateOptions.KeyStorageFlags = X509KeyStorageFlags.EphemeralKeySet | X509KeyStorageFlags.MachineKeySet;
             var certificate = keyVaultCertificateClient.DownloadCertificate(downloadCertificateOptions);
             return certificate.Value;
+        }
+
+        private static async Task<Stream> GetUploadedPhotoFromStorageAsStream(EventGridEvent eventGridEvent, BlobServiceClient blobServiceClient)
+        {
+            var url = eventGridEvent.Data.ToObjectFromJson<StorageBlobCreatedEventData>().Url;
+            var fileName = url.Split("/")[url.Split("/").Length - 1];
+            var blobContainerClient = GetBlobContainerClient(blobServiceClient, Environment.GetEnvironmentVariable("uploadContainerName"));
+            logger.LogInformation("File name: " + fileName);
+            var blobClient = blobContainerClient.GetBlobClient(fileName);
+            logger.LogInformation("Reading from storage: " + blobContainerClient.Uri);
+            var binaryData = await blobClient.OpenReadAsync();
+            return binaryData;
         }
     }
 }
